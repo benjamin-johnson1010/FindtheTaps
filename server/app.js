0ffee4161c93a6bc506d79a14c921b390c50d816
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2016-10-02 22:52:39 -0500

    started template for server route

---
+++ server/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+//Database connection
+var connection = require('../modules/connection');
+mongoose.connect(connection);
+
+//body parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// require and use index router
+var index = require('../routers/index');
+app.use('/', index);
+
+//require and user pets router
+var pets = require('../routers/');
+app.use('/', );
+
+// static files
+app.use(express.static('public'));
+
+// server listen port
+var portDecision = process.env.PORT || 3000;
+
+var server = app.listen(portDecision, function() {
+  var port = server.address().port;
+  console.log('Listening on', port);
+});
