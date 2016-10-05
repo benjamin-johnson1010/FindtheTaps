cbd87b5523686f4c3cac15b490b257efcbd9b9ae
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2016-10-05 09:31:27 -0500

    api for craft brewing search is working

--- public/scripts/controllers/searchController.js
+++ public/scripts/controllers/searchController.js
@@ -1,7 +1,9 @@
 var key = '&key=AIzaSyAgt1boABfcHeikPyY8Xps1SD5JLfepjcw';
 var geoURL = 'https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:';
-// var placesURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=';
-// var radiusURL='&radius'
+var placesURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=';
+var radiusURL='&radius=16093.4';
+//checks for brewery and taphouse to refine results, which eliminates restaurants
+var breweryURL= '&keyword=brewery|taproom|brewing|taphouse=';
 myApp.controller("searchController", ["$scope", "$http", function($scope,$http){
   console.log("OH HAI SEARCH");
 $scope.newSearch=function(){
@@ -15,10 +17,20 @@ $http({
 //get latitude
 $scope.lat =$scope.newLocation =mapData.data.results[0].geometry.location.lat;
 //get longitude
-$scope.long=$scope.newLocation =mapData.data.results[0].geometry.location.lng;
+$scope.lng=$scope.newLocation =mapData.data.results[0].geometry.location.lng;
+console.log($scope.lat);
+console.log($scope.lng);
+$scope.searchArea = placesURL + $scope.lat + ',' + $scope.lng + radiusURL + breweryURL + key;
+console.log($scope.searchArea);
   $http({
-    url: $scope.serachLocation,
-  })
+    url: $scope.searchArea,
+    datatype: JSON
+  }).then(function(response){
+//returns data back from server
+    console.log(response.data.results);
+//list equals the array of objects of brewery and taphouse
+    $scope.list = response.data.results;
+  });//then function end for 2nd http call
   });//end http for geo call
 };//end newSearch
 }]);//end searchController
