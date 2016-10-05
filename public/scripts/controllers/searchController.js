c9e86c739be09091bca319ee60f4b60492dbd16c
BENJAMIN JOHNSON <wibajohnson@gmail.com>
2016-10-05 16:31:31 -0500

    have search controller, html finished for time being

--- public/scripts/controllers/searchController.js
+++ public/scripts/controllers/searchController.js
@@ -1,7 +1,9 @@
 var key = '&key=AIzaSyAgt1boABfcHeikPyY8Xps1SD5JLfepjcw';
 var geoURL = 'https://maps.googleapis.com/maps/api/geocode/json?components=postal_code:';
 var placesURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=';
-var radiusURL='&radius=40233.6&hasNextPage=true&nextPage()=true';
+var radius='&radius=';
+var next = '&hasNextPage=true&nextPage()=true';
+
 //checks for brewery and taphouse to refine results, which eliminates restaurants
 var breweryURL= '&keyword=brewery|taproom|brewing|taphouse=';
 myApp.controller("searchController", ["$scope", "$http", function($scope,$http){
@@ -19,17 +21,34 @@ $scope.lat =$scope.newLocation =mapData.data.results[0].geometry.location.lat;
 $scope.lng=$scope.newLocation =mapData.data.results[0].geometry.location.lng;
 console.log($scope.lat);
 console.log($scope.lng);
-$scope.searchArea = placesURL + $scope.lat + ',' + $scope.lng + radiusURL + breweryURL + key;
+if ($scope.miles == 5) {
+  $scope.distance = 8046.72;
+}
+else if($scope.miles == 10){
+$scope.distance = 16093.4;
+}
+else if($scope.miles == 25){
+  $scope.distance = 40233.6;
+}
+else{
+  alert("Please select a value");
+}
+console.log($scope.distance);
+$scope.searchArea = placesURL + $scope.lat + ',' + $scope.lng + radius + $scope.distance + next + breweryURL + key;
   $http({
     url: $scope.searchArea,
     datatype: JSON
   }).then(function(response){
 //returns data back from server
-console.log(response);
-    console.log(response.data.results);
 //list equals the array of objects of brewery and taphouse
     $scope.list = response.data.results;
+    console.log($scope.list);
   });//then function end for 2nd http call
   });//end http for geo call
 };//end newSearch
+$scope.viewLocation=function(data){
+  console.log('in viewLocation', data);
+
+
+};
 }]);//end searchController
